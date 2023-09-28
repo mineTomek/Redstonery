b59de9583ac602865eea206db2aa49d4f702f385
mineTomek <lukom.tomek@gmail.com>
2023-09-28 16:16:32 +0200

    Use a state for storing the blocks

--- src/app/components/simulation/Simulator.tsx
+++ src/app/components/simulation/Simulator.tsx
@@ -9,6 +9,14 @@ import SimBlock from './SimBlock'
 export default function Simulator() {
   const [autoRotate, setAutoRotate] = useState(true)
 
+  const [blocks, setBlocks] = useState<SimBlock[]>([
+    new SimBlock(0, 0, 0, 0x6dc53b),
+    new SimBlock(1, 0, 0, 0xc56d3b),
+    new SimBlock(0, 0, 1, 0xc53b6d),
+    new SimBlock(1, 0, 1, 0x6d3bc5),
+    new SimBlock(0, 1, 0, 0x6d3bc5),
+  ])
+
   return (
     <Canvas>
       <ambientLight />
@@ -21,9 +29,7 @@ export default function Simulator() {
         autoRotateSpeed={2}
         onStart={() => setAutoRotate(false)}
       />
-      <Grid
-        blocks={[new SimBlock(0, 0, 0, 0x6dc53b)]}
-      />
+      <Grid blocks={blocks} />
     </Canvas>
   )
 }
