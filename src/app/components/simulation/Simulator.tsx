5f5a173bf9b17723a7c00f3bb58e3ce727d04802
mineTomek <lukom.tomek@gmail.com>
2023-09-28 12:24:24 +0200

    Add controls and autorotation

--- src/app/components/simulation/Simulator.tsx
+++ src/app/components/simulation/Simulator.tsx
@@ -1,15 +1,24 @@
 'use client'
 
+import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { useState } from 'react'
 import Grid from './Grid'
 
 export default function Simulator() {
+  const [autoRotate, setAutoRotate] = useState(true)
+
   return (
     <Canvas>
       <ambientLight />.
       <pointLight
         position={[10, 10, 10]}
         intensity={2 * 500}
+      />
+      <OrbitControls
+        autoRotate={autoRotate}
+        autoRotateSpeed={2}
+        onStart={() => setAutoRotate(false)}
       />
       <Grid
         width={3}
