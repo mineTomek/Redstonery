bb7e341c40a308a6e5917f09e5a706d87b97ad0b
mineTomek <lukom.tomek@gmail.com>
2023-09-28 21:46:59 +0200

    Add more properties to SimBlock class

--- src/app/components/simulation/SimBlock.ts
+++ src/app/components/simulation/SimBlock.ts
@@ -5,10 +5,28 @@ export default class SimBlock {
 
   public color: number = 0xffffff
 
+  public facing: Facing = Facing.None
+
+  public state: boolean = false
+
+  public locked: boolean = false
+
+  public subtract: boolean = false
+
   constructor(x: number, y: number, z: number, color: number) {
     this.x = x
     this.y = y
     this.z = z
     this.color = color
   }
+}
+
+export enum Facing {
+  None,
+  Up,
+  Down,
+  North,
+  East,
+  South,
+  West,
 }
