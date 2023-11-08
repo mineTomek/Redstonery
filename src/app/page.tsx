8e07e1e1458c3d65a6c6fae797969d1695d04ed1
mineTomek <lukom.tomek@gmail.com>
2023-11-08 17:57:25 +0100

    Adjust the sizes on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
     <main className='bg-white min-h-screen'>
       <Navbar />
 
-      <div className='inline-flex flex-col items-center pb-6 w-full h-screen justify-between'>
-        <Simulator circuit='clock' />
-        <div className='self-stretch text-center text-[28px] font-black text-slate-800'>
+      <div className='inline-flex flex-col items-center pb-6 gap-3 w-full h-screen justify-between'>
+        <Simulator circuit='clock' className="[height:40rem!important;]"/>
+        <div className='self-stretch text-center text-[28px] font-black text-slate-800 px-4'>
           Simulating redstone since... not yet.
         </div>
-        <div className='w-[340px] text-center text-base font-black text-slate-700'>
+        <div className='w-[340px] text-center text-lg font-black text-slate-700'>
           A redstone simulator that helps you create better redstone contraptions
         </div>
         <Button
