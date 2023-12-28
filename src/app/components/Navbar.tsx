398824de644391b1c97e157cbfde916f84d6637b
mineTomek <lukom.tomek@gmail.com>
2023-12-28 11:41:47 +0100

    Add a gradient background to the navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className='h-16 fixed top-0 left-0 right-0 px-4 flex items-center'>
+    <div className='h-16 fixed top-0 left-0 right-0 px-4 flex items-center bg-gradient-to-r from-white dark:from-gray-950 to-transparent z-10 w-1/2'>
       <h3>Redstonery</h3>
     </div>
   )
